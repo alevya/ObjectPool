92bfb55c7b0492b512c7a7c7606cd9248f717515
Aleksey Vyaznikov <alexv47@mail.ru>
2017-12-08 12:56:37 +0300

    add class lib

---
+++ TestMain/ObjectPool/ObjectPool.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObjectPool
+{
+    public abstract class ObjectPool<T>
+    {
+        private readonly int _expirationTime;
+        private IDictionary<T, int> _locked;
+        private readonly IDictionary<T, int> _unlocked;
+
+        protected ObjectPool()
+        {
+            _expirationTime = 10 * 1000;
+            _locked = new ConcurrentDictionary<T, int>();
+            _unlocked = new ConcurrentDictionary<T, int>();
+        }
+
+        protected abstract T _create();
+
+        public abstract bool Validate(T obj);
+        public abstract void Expire(T obj);
+
+        public T CheckOut()
+        {
+            T t = default(T);
+            int now = DateTime.Now.Millisecond;
+            if (_unlocked.Any())
+            {
+                foreach (var unlockedKey in _unlocked.Keys)
+                {
+                    t = unlockedKey;
+                    int value;
+                    if(_unlocked.TryGetValue(unlockedKey, out value)) continue;
+
+                    if ((now - value) > _expirationTime)
+                    {
+                        _unlocked.Remove(unlockedKey);
+                        Expire(t);
+                    }
+                    else
+                    {
+                        if (Validate(t))
+                        {
+                            _unlocked.Remove(t);
+                            _locked.Add(t, now);
+                            return t;
+                        }
+                        _unlocked.Remove(t);
+                        Expire(t);
+                        t = default(T);
+                    }
+                }
+            }
+
+            t = _create();
+            _locked.Add(t, now);
+
+            return t;
+        }
+
+        public void CheckIn(T t)
+        {
+            
+        }
+
+    }
+}
